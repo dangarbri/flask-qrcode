dfdc044291067870d4b7a05de837a40007bd1049
Daniel Garcia Briseno <dg.briseno@gmail.com>
2024-03-29 21:40:44 -0400

    remove tempfile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request
 import segno
 import tempfile
 import base64
@@ -12,17 +12,18 @@ def generate_qr_code():
     if text:
         try:        
             qr = segno.make(text)
-            temp_file = tempfile.NamedTemporaryFile(delete_on_close=False)
+            temp_file = tempfile.NamedTemporaryFile()
+            temp_file.close()
             tmp = temp_file.name + ".png"
             qr.save(tmp, scale=20)
-            temp_file.close()
 
             with open(tmp, 'rb') as file:
                 qr_data = file.read()
                 base64_encoded = base64.b64encode(qr_data).decode('utf-8')
+            os.remove(tmp)
 
             return f'data:image/png;base64,{base64_encoded}"', 200, {'Cache-Control': 'max-age=31536000'}
-        except:
+        except Exception as e:
             return "An error occurred while generating the QR code.", 500
     else:
         return "Please provide a 'text' query parameter.", 400
